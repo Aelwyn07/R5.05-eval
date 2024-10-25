1a3fc2b22d4dcc42925bdc11d0c99f3f349ed241
Mathis Krizmanic <mathis.krizmanic@gmail.com>
2024-10-25 16:13:28 +0200

    corrections légères

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -6,8 +6,12 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Models\Categorie;
 
 class CategoriesController extends Controller
 {
-
+    public function create() {
+        $categories = Categorie::all();
+        return view('app.categories.show', compact('categories'));
+    }
 }
